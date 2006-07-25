16fcaf9704fdf388c9110d66e6d6130747479316
Apple OSS Distributions <91980991+AppleOSSDistributions@users.noreply.github.com>
2006-07-25 19:50:18 +0000

    OpenSSH-57.3
    Imported from OpenSSH-57.3.tar.gz

--- openssh/audit.h
+++ openssh/audit.h
@@ -1,6 +1,7 @@
+/* $Id: audit.h,v 1.2 2005/02/08 10:52:48 dtucker Exp $ */
+
 /*
- * Copyright 1993-2002 Sun Microsystems, Inc.  All rights reserved.
- * Use is subject to license terms.
+ * Copyright (c) 2004, 2005 Darren Tucker.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -21,40 +22,35 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
  */
 
-#ifndef	_BSD_SOLARIS_H
-#define	_BSD_SOLARIS_H
+#include "auth.h"
 
-#if defined(HAVE_BSM_AUDIT_H) && defined(HAVE_LIBBSM)
+#ifndef _SSH_AUDIT_H
+# define _SSH_AUDIT_H
+enum ssh_audit_event_type {
+	SSH_LOGIN_EXCEED_MAXTRIES,
+	SSH_LOGIN_ROOT_DENIED,
+	SSH_AUTH_SUCCESS,
+	SSH_AUTH_FAIL_NONE,
+	SSH_AUTH_FAIL_PASSWD,
+	SSH_AUTH_FAIL_KBDINT,	/* keyboard-interactive or challenge-response */
+	SSH_AUTH_FAIL_PUBKEY,	/* ssh2 pubkey or ssh1 rsa */
+	SSH_AUTH_FAIL_HOSTBASED,	/* ssh2 hostbased or ssh1 rhostsrsa */
+	SSH_AUTH_FAIL_GSSAPI,
+	SSH_INVALID_USER,
+	SSH_NOLOGIN,		/* denied by /etc/nologin, not implemented */
+	SSH_CONNECTION_CLOSE,	/* closed after attempting auth or session */
+	SSH_CONNECTION_ABANDON,	/* closed without completing auth */
+	SSH_AUDIT_UNKNOWN
+};
+typedef enum ssh_audit_event_type ssh_audit_event_t;
 
-#pragma ident	"@(#)bsmaudit.h	1.1	01/09/17 SMI"
+void	audit_connection_from(const char *, int);
+void	audit_event(ssh_audit_event_t);
+void	audit_session_open(const char *);
+void	audit_session_close(const char *);
+void	audit_run_command(const char *);
+ssh_audit_event_t audit_classify_auth(const char *);
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
-
-#include <bsm/audit.h>
-#define	AUE_openssh	32800
-
-void solaris_audit_maxtrys(void);
-void solaris_audit_nologin(void);
-void solaris_audit_save_name(const char *name);
-void solaris_audit_save_pw(struct passwd *pwd);
-void solaris_audit_not_console(void);
-void solaris_audit_bad_pw(const char *what);
-void solaris_audit_save_host(const char *host);
-void solaris_audit_save_ttyn(const char *ttyn);
-void solaris_audit_save_port(int port);
-void solaris_audit_save_command(const char *command);
-void solaris_audit_success(void);
-void solaris_audit_logout(void);
-
-#ifdef	__cplusplus
-}
-#endif
-
-#endif /* BSM */
-
-#endif	/* _BSD_SOLARIS_H */
+#endif /* _SSH_AUDIT_H */
--- openssh/openbsd-compat/port-uw.h
+++ openssh/openbsd-compat/port-uw.h
@@ -1,18 +1,5 @@
-/* $OpenBSD: sshtty.h,v 1.3 2003/09/19 17:43:35 markus Exp $ */
 /*
- * Author: Tatu Ylonen <ylo@cs.hut.fi>
- * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
- *                    All rights reserved
- *
- * As far as I am concerned, the code I have written for this software
- * can be used freely for any purpose.  Any derived versions of this
- * software must be clearly marked as such, and if the derived work is
- * incompatible with the protocol description in the RFC file, it must be
- * called by a name other than "ssh" or "Secure Shell".
- */
-/*
- * Copyright (c) 2001 Markus Friedl.  All rights reserved.
- * Copyright (c) 2001 Kevin Steves.  All rights reserved.
+ * Copyright (c) 2005 Tim Rice.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -35,13 +22,9 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef SSHTTY_H
-#define SSHTTY_H
+#include "includes.h"
 
-#include <termios.h>
-
-struct termios get_saved_tio(void);
-void	 leave_raw_mode(void);
-void	 enter_raw_mode(void);
-
+#if defined(HAVE_LIBIAF)  &&  !defined(BROKEN_LIBIAF)
+char * get_iaf_password(struct passwd *pw);
 #endif
+
